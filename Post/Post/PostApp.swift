4996b0f8992d0896b69555f25a8b7f69a531e5be
Sanjay Kumar <sanjaykumarmurthy@gmail.com>
2022-09-13 06:51:49 +0530

    iOS swiftUI project created with test

---
+++ Post/Post/PostApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostApp.swift
+//  Post
+//
+//  Created by Kumar, Sanjay (623) on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
