79877abd2523cf14a7b0547ff7b01c7edecd0163
Sanjay Kumar <sanjaykumarmurthy@gmail.com>
2022-10-06 22:37:14 +0530

    feat: HTTPClientSpy create

---
+++ Post/PostTests/Networking/TestHelpers/HTTPClientSpy.swift
@@ -0,0 +1,47 @@
+//
+//  HTTPClientSpy.swift
+//  PostTests
+//
+//  Created by Kumar, Sanjay (623) on 06/10/22.
+//
+
+import XCTest
+@testable import Post
+
+class HTTPClientSpy: HTTPClient {
+    
+    private struct Task: HTTPClientTask {
+        let callback: () -> Void
+        func cancel() {
+            callback()
+        }
+    }
+    
+    private var messages = [(request: URLRequest, completion: (HTTPClient.Result) -> Void)]()
+    private (set) var cancelledURL: [URL?] = []
+    
+    var requestedURL: [URL] {
+        messages.compactMap { $0.request.url }
+    }
+    
+    func dispatch(_ request: URLRequest, completion: @escaping DispatchCompletion) -> HTTPClientTask {
+        messages.append((request, completion))
+        return Task { [weak self] in
+            self?.cancelledURL.append(request.url)
+        }
+    }
+    
+    func complete(with error: Error,at index: Int = 0) {
+        messages[index].completion(.failure(error))
+    }
+    
+    func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
+        let response = HTTPURLResponse(
+            url: requestedURL[index],
+            statusCode: code,
+            httpVersion: nil,
+            headerFields: nil
+        )!
+        messages[index].completion(.success((data, response)))
+    }
+}
