e17627ff28a0e7e918f48d54dbd8d02146c688b5
Sanjay Kumar <sanjaykumarmurthy@gmail.com>
2022-10-27 15:25:36 +0530

    feat: add endpoint for feed

---
+++ Post/Post/Helpers/Endpoints.swift
@@ -0,0 +1,15 @@
+//
+//  Endpoints.swift
+//  Post
+//
+//  Created by Kumar, Sanjay (623) on 27/10/22.
+//
+
+import Foundation
+
+extension Endpoint {
+    static var PAGE_SIZE: Int { 25 }
+    static func feed(page: Int = 0, size: Int = PAGE_SIZE) -> Self {
+        self.init(host: "dummy", path: ["data","v1","post"], queryItems: ["limit": "\(size)"])
+    }
+}
