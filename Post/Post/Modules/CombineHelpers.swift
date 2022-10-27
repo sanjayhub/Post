f38ff4ff2f22ad742d4db79b02d72938deffb619
Sanjay Kumar <sanjaykumarmurthy@gmail.com>
2022-10-27 14:49:13 +0530

    feat: create `DispatchQueue` helper to ensure requests dispatch on main queue.

--- Post/Post/Modules/CombineHelpers.swift
+++ Post/Post/Modules/CombineHelpers.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Combine
 
-// MARK:- FeedViewModel
+// MARK: - FeedViewModel
 extension FeedViewModel {
     convenience init(loadFeedPublisher publisher: @escaping () -> AnyPublisher<[Feed], Error> ) {
         self.init(loader: { completion in
@@ -18,5 +18,58 @@ extension FeedViewModel {
                     receiveValue: { result in completion(.success(result)) }
                 ))
         })
+    }
+}
+
+// MARK: - DispatchQueue
+
+extension DispatchQueue {
+    
+    static var immediateWhenOnMainQueueScheduler: ImmediateWhenOnMainQueueScheduler {
+        ImmediateWhenOnMainQueueScheduler.shared
+    }
+    
+    struct ImmediateWhenOnMainQueueScheduler: Scheduler {
+        typealias SchedulerTimeType = DispatchQueue.SchedulerTimeType
+        
+        typealias SchedulerOptions = DispatchQueue.SchedulerOptions
+        
+        var now: SchedulerTimeType {
+            DispatchQueue.main.now
+        }
+        
+        static let shared = Self()
+        
+        var minimumTolerance: SchedulerTimeType.Stride {
+            DispatchQueue.main.minimumTolerance
+        }
+        
+        private static let key = DispatchSpecificKey<UInt8>()
+        private static let value = UInt8.max
+        
+        private init() {
+            DispatchQueue.main.setSpecific(key: Self.key, value: Self.value)
+        }
+        
+        private func isMainQueue() -> Bool {
+            
+            return DispatchQueue.getSpecific(key: Self.key) == Self.value
+        }
+        
+        func schedule(options: SchedulerOptions?, _ action: @escaping () -> Void) {
+            guard isMainQueue() else {
+                return DispatchQueue.main.schedule(options: options, action)
+            }
+            action()
+        }
+        
+        func schedule(after date: SchedulerTimeType, tolerance: SchedulerTimeType.Stride, options: SchedulerOptions?, _ action: @escaping () -> Void) {
+            DispatchQueue.main.schedule(after: date, tolerance: tolerance, options: options, action)
+        }
+        
+        func schedule(after date: SchedulerTimeType, interval: SchedulerTimeType.Stride, tolerance: SchedulerTimeType.Stride, options: SchedulerOptions?, _ action: @escaping () -> Void) -> Cancellable {
+            DispatchQueue.main.schedule(after: date, interval: interval, tolerance: tolerance, action)
+        }
+        
     }
 }
