609a73dc95a5f03bac577c9d548d8cda0adaf1b0
Sanjay Kumar <sanjaykumarmurthy@gmail.com>
2022-11-05 11:04:14 +0530

    feat: create placeholder `AsyncImageView`

---
+++ Post/Post/Modules/Common/UI/Views/AsyncImage/AsyncImageView.swift
@@ -0,0 +1,20 @@
+//
+//  AsyncImageView.swift
+//  Post
+//
+//  Created by Kumar, Sanjay (623) on 03/11/22.
+//
+
+import SwiftUI
+
+struct AsyncImageView: View {
+    var body: some View {
+        Color(.tertiaryLabel)
+    }
+}
+
+struct AsyncImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        AsyncImageView()
+    }
+}
--- Post/Post/Modules/Feed/UI/Views/FeedListView.swift
+++ Post/Post/Modules/Feed/UI/Views/FeedListView.swift
@@ -14,8 +14,6 @@ struct FeedListView: View {
     var body: some View {
         
         renderViewFor(viewModel.state)
-            .onAppear(perform: viewModel.loadFeed)
-            .padding()
     }
 }
 
@@ -27,12 +25,15 @@ private extension FeedListView {
                 LoadingView()
                     .frame(width: 50.0, height: 50.0)
                     .foregroundColor(Color("3dc6a7"))
+                    .onAppear(perform: viewModel.loadFeed)
                 
             case let .loaded(feed):
                 ScrollView(.vertical, showsIndicators: true) {
                     LazyVStack(spacing: 0) {
                         ForEach(feed, id: \.id) { item in
-                            Text(item.id)
+                            AsyncImageView()
+                                .frame(height: 400)
+                                .shimmer()
                         }
                     }
                 }
